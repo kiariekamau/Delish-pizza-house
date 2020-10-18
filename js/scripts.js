aadb144a8cfc6967815c36681992d76f09e3d662
kiarie <denniskiarie426@gmail.com>
2020-10-18 18:41:28 +0300

    add to the user input in scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -25,7 +25,56 @@ function crustPrice () {
     return pizzaCrustPrice
 }
 function toppingsPrice () {
-    var toppings =$("toppings").val();
+    var toppings =$("#toppings").val();
     var pizzaToppingPrice=  100;
     return pizzaToppingPrice
 }
+function numberOfPizzaPrice () {
+    var numberOfPizza = $("#numberOfPizza").val();
+    return parseInt(numberOfPizza);
+}
+function pizzaPrice () {
+    var totalPrice = parseInt(sizePrice()+ crustPrice()+ toppingsPrice()) + numberOfPizzaPrice();
+    console.log(totalPrice);
+
+    $("#price").text(+ totalPrice)
+}
+
+function Pizza( type,size,crust,topping,numberOfPizzas) {
+    this.type = type
+    this.size = size
+    this.crust = crust
+    this.topping = topping
+    this.numberOfPizzas = numberOfPizzas
+}
+
+Pizza.prototype.addEverything = function () {
+    return this.type + "," + this.size + "," + this.crust + "," + this.topping + "," + this.numberOfPizzas 
+}
+
+function checkout () {
+    alert("Your order has been received!");
+}
+
+$(document).ready(function(){
+    $("#pickUp").click(function(event){
+        event.preventDefault();
+
+        $("#pickUp").show();
+        $("#delivery").hide();
+
+     var type = $("#pizzaType").val();
+     var size = $("#pizzaSize").val();
+     var crust = $("#pizzaCrust").val();
+     var topping = $("#toppings").val();
+     var numberOfPizzas =$("#numberOfPizzas").val();
+
+     var pizzaOrder = new Pizza(type, size, crust, topping, numberOfPizzas)
+
+     $("#pizzaForPickUp").show();
+     $("#pizzaForDelivery").hide();
+     $("ul#pizzaForPickUp").append("<li>" + pizzaOrder.addEverything() + "<li>");
+    })
+    
+
+})
